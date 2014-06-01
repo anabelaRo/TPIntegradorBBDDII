7b0d2b1ab40744574c7e00bddf2b5d270d264b37
Anabela <anabela.rossi@gmail.com>
2014-06-01 12:14:48 -0300

    Punto 6
    Se añaden algunos inserts en el punto 4

---
+++ Punto6_CreateAux_Nota.sql
@@ -0,0 +1,18 @@
+SELECT * INTO tp1.aux_nota  
+FROM  tp1.nota
+WHERE codmat = 2 /*BASES DE DATOS I*/
+AND nota < 4     /*Se aprueba con 4*/
+
+ALTER TABLE tp1.aux_nota WITH CHECK ADD  CONSTRAINT FK_AuxNota_Materia FOREIGN KEY(codmat)
+REFERENCES tp1.materia(codmat)
+GO
+ALTER TABLE tp1.aux_nota CHECK CONSTRAINT FK_AuxNota_Materia
+GO
+
+ALTER TABLE tp1.aux_nota WITH CHECK ADD  CONSTRAINT FK_AuxNota_Alumno FOREIGN KEY(codigoal)
+REFERENCES tp1.alumno(codigoal)
+GO
+ALTER TABLE tp1.aux_nota CHECK CONSTRAINT FK_AuxNota_Alumno
+GO
+
+select * from tp1.aux_nota /*query para validar el create*/
