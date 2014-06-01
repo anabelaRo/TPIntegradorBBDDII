777ca39da780124eb936a2551b1a5c82f285cd96
emilianozeta <emiliano.zambrano@hotmail.com>
2014-06-01 17:12:12 -0300

    Punto 1 modif.

--- ParteSQL/Punto1.sql
+++ ParteSQL/Punto1.sql
@@ -1,13 +1,17 @@
-SELECT alu.nombreal AS Nombre, car.nombrecarr AS Carrera, mat.nombremat AS Materia, nota.calif AS Calificación, nota.fecha AS Fecha
-FROM tp1.alumno AS alu
-INNER JOIN tp1.mONitor AS mON
-	ON alu.codigoal = mON.codigoal
-INNER JOIN tp1.materia mat
-	ON mON.codmat = mat.codmat
-	AND mat.codmat = mON.codmat
-INNER JOIN tp1.carrera AS car
-	ON mat.carrera = car.codcarr
-INNER JOIN tp1.nota AS nota
-	ON alu.codigoal = nota.codigoal
-	AND mat.codmat = nota.codmat
-ORDER BY alu.nombreal,  car.nombrecarr, mat.nombremat
+SELECT alu.nombreal   AS Nombre,
+       car.nombrecarr AS Carrera,
+       mat.nombremat  AS Materia,
+       nota.calif     AS Calificación,
+       nota.fecha     AS Fecha
+  FROM tp1.alumno AS alu
+       INNER JOIN tp1.monitor AS mon
+	      ON alu.codigoal = mon.codigoal
+       INNER JOIN tp1.materia mat
+          ON mon.codmat = mat.codmat
+         AND mat.codmat = mon.codmat
+       INNER JOIN tp1.carrera AS car
+          ON mat.carrera = car.codcarr
+       INNER JOIN tp1.nota AS nota
+          ON alu.codigoal = nota.codigoal
+         AND mat.codmat = nota.codmat
+ORDER BY alu.nombreal, car.nombrecarr, mat.nombremat
