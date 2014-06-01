1fc344e58a15663988c805b79bfa0932f0dfcf0a
Anabela <anabela.rossi@gmail.com>
2014-06-01 15:23:50 -0300

    Se agrega el punto 2

--- DML/Punto1_Inserts.sql
+++ DML/Punto1_Inserts.sql
@@ -3,7 +3,6 @@ insert into tp1.carrera values (2, 'Educación Física')
 insert into tp1.carrera values (3, 'Ingeniería Industrial')
 insert into tp1.carrera values (4, 'Tecnicatura Web')
 
-insert into tp1.alumno values (1, 'Carlos Alberto García Moreno', 1, 'Tucumán 123', 'Morón', 'Buenos Aires', 1708)
 insert into tp1.alumno values (2, 'Juan Pérez', 3, 'Av Juan B Justo 2563', 'Villa Crespo', 'Buenos Aires', 1414 )
 insert into tp1.alumno values (3, 'Maria Gonzalez', 3, 'Av Rivadavia 15345', 'Flores', 'Buenos Aires', 1418 )
 insert into tp1.alumno values (4, 'Emiliano Zambrano', 4, 'Facundo 2341', 'Nueva', 'Buenos Aires', 1701 )
@@ -47,3 +46,5 @@ insert into tp1.nota values (10, 1, '19990110', 7)
 insert into tp1.nota values (11, 1, '19990215', 8.5)
 insert into tp1.nota values (12, 1, '20000215', 9)
 insert into tp1.nota values (13, 1, '20010215', 9)
+
+insert into tp1.alumno values (1, 'Carlos Alberto García Moreno', 1, 'Tucumán 123', 'Morón', 'Buenos Aires', 1708) /*punto 2*/
