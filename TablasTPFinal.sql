461f4938c8692660ad11fe787cdc0634b7259e5c
emilianozeta <emiliano.zambrano@hotmail.com>
2014-06-01 11:15:04 -0300

    Esquema actualizado

--- TablasTPFinal.sql
+++ TablasTPFinal.sql
@@ -1,19 +1,19 @@
-create schema tpfinal
+create schema tp1
 
-create table tpfinal.alumno
+create table tp1.alumno
 (
 codigoal smallint not null,
 nombreal varchar(32) not null,
 carrera varchar(32) not null,
 CONSTRAINT PK_alumno_codigoal PRIMARY KEY CLUSTERED (codigoal))
 
-create table tpfinal.monitor
+create table tp1.monitor
 (
 codigoal smallint not null,
 codmat smallint not null,
 CONSTRAINT PK_monitor_codigoal_codmat PRIMARY KEY CLUSTERED (codigoal, codmat))
 
-create table tpfinal.profesor
+create table tp1.profesor
 (
 codprof smallint not null,
 nombre varchar(32) not null,
@@ -21,17 +21,18 @@ puesto varchar(32) not null,
 carrera varchar(32) not null,
 CONSTRAINT PK_profesor_codprof PRIMARY KEY CLUSTERED (codprof))
 
-create table tpfinal.materia
+create table tp1.materia
 (
 codmat smallint not null,
 nombremat varchar(32) not null,
 carrera varchar(32) not null,
 CONSTRAINT PK_materia_codmat PRIMARY KEY CLUSTERED (codmat))
 
-create table tpfinal.nota
+create table tp1.nota
 (
 codmat smallint not null,
 codigoal smallint not null,
 fecha date not null,
-nota DECIMAL(4,2) not null, /*poner dos digitos*/
+nota DECIMAL(4, 2) not null, /*poner dos digitos*/
 CONSTRAINT PK_nota_codmat_codigoal PRIMARY KEY CLUSTERED (codmat, codigoal))
+
