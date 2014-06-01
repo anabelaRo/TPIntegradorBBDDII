b16b2d3fad0d50a66debc7e0b4340b0587d5c0c9
Anabela <anabela.rossi@gmail.com>
2014-06-01 13:27:49 -0300

    Inserts

---
+++ DML/Punto1_Inserts.sql
@@ -0,0 +1,11 @@
+insert into tp1.carrera values (1, 'Medicina')
+insert into tp1.carrera values (2, 'Educación Física')
+insert into tp1.carrera values (3, 'Ingeniería Industrial')
+insert into tp1.carrera values (4, 'Tecnicatura Web')
+
+insert into tp1.alumno values (1, 'Carlos Alberto García Moreno', 1, 'Tucumán 123', 'Morón', 'Buenos Aires', 1708)
+insert into tp1.alumno values (2, 'Juan Pérez', 3, 'Av Juan B Justo 2563', 'Villa Crespo', 'Buenos Aires', 1414 )
+insert into tp1.alumno values (3, 'Maria Gonzalez', 3, 'Av Rivadavia 15345', 'Flores', 'Buenos Aires', 1418 )
+insert into tp1.alumno values (2, 'Juan Pérez', 3, 'Av Juan B Justo 2563', 'Villa Crespo', 'Buenos Aires', 1414 )
+insert into tp1.alumno values (4, 'Emiliano Zambrano', 4, 'Facundo 2341', 'Nueva', 'Buenos Aires', 1701 )
+
