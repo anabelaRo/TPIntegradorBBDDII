d3b115921a5ee32976ce8faf9ec06da1c944b3f8
Anabela <anabela.rossi@gmail.com>
2014-06-01 17:05:50 -0300

    Revert "2"
    This reverts commit 45a70675936c4f04b4814b6cc3ed8d1a3109d801.

--- DML/Punto10.sql
+++ DML/Punto10.sql
@@ -1,9 +1,19 @@
-SELECT alu.codigoal AS Alumno, AVG(nota.calif) AS Promedio
-FROM tp1.nota AS nota
+--Mostrar el código de alumno y el promedio de las materias aprobadas siempre que haya aprobado 5 ó más materias.
+
+SELECT alu.codigoal, alu.nombreal , mon.codmat, nota.calif
+FROM tp1.alumno AS alu
 	INNER JOIN tp1.monitor AS mon
-		ON nota.codmat = mon.codmat
-	INNER JOIN tp1.alumno AS alu
-		ON mon.codigoal = alu.codigoal
+		ON alu.codigoal = mon.codigoal
+	INNER JOIN tp1.nota AS nota
+		ON mon.codmat = nota.codmat
 WHERE nota.calif >= 4
-GROUP BY alu.codigoal
-HAVING COUNT(mon.codmat) > 5;
+GROUP BY alu.codigoal;
+
+SELECT *
+FROM tp1.alumno;
+
+SELECT *
+FROM tp1.monitor;
+
+SELECT *
+FROM tp1.nota;
